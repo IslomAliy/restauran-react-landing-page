fd6b804272495e657cd9f59b9cf06a0bfe82b5dc
islomaliy <kislombek71@gmail.com>
2021-07-15 16:37:35 +0500

    Carousel fixed

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,24 +1,14 @@
-import styled from "styled-components";
-
-const Images = styled.div `
-    display: flex;
-    overflow: hidden;
-
-    .image {
-        margin-right: 2px;
-    }
-
-`;
+import { Images } from "./style";
 
 const Carousel = () => {
     return ( 
         <Images>
-            <img className="image" src="images/bottom.png" alt="bottom-gallery" />
-            <img className="image" src="images/bottom.png" alt="bottom-gallery" />
-            <img className="image" src="images/bottom.png" alt="bottom-gallery" />
-            <img className="image" src="images/bottom.png" alt="bottom-gallery" />
-            <img className="image" src="images/bottom.png" alt="bottom-gallery" />
-            <img className="image" src="images/bottom.png" alt="bottom-gallery" />
+            <img src="images/bottom.png" alt="bottom-gallery" />
+            <img src="images/bottom.png" alt="bottom-gallery" />
+            <img src="images/bottom.png" alt="bottom-gallery" />
+            <img src="images/bottom.png" alt="bottom-gallery" />
+            <img src="images/bottom.png" alt="bottom-gallery" />
+            <img src="images/bottom.png" alt="bottom-gallery" />
         </Images>
         
      );
---
+++ src/components/Carousel/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Images = styled.div`
+    display: flex;
+    overflow: hidden;
+
+    img {
+        margin-right: 2px;
+    }
+
+`;
