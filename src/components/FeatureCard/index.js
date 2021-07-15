c5cac96517be216a91020911206fc7a2f5af3ab5
islomaliy <kislombek71@gmail.com>
2021-07-15 19:31:45 +0500

    FeatureCard minimized

--- src/components/FeatureCard/index.js
+++ src/components/FeatureCard/index.js
@@ -1,35 +1,16 @@
-import styled from "styled-components";
-
-const StyledHeader = styled.div `
-    display: flex;
-    /* justify-content: space-between; */
-    align-items: center;
-    margin-bottom: 20px;
-`;
-
-const StyledImage = styled.img`
-    margin-right: 20px;
-`;
-
-const StyledTitle = styled.h3`
-    margin-top: 11px;
-`;
-
-const StyledDesc = styled.p`
-    color: #616161;
-    font-size: 14px;
-    font-weight: 300;
-    line-height: 18.9px;
-`;
+import { Header, 
+        Image, 
+        Title, 
+        Desc } from "./style";
 
 const FeatureCard = ({ className, img, title, desc }) => {
     return ( 
         <div className={className}>
-            <StyledHeader>
-                <StyledImage src={img} alt="desc 1 logo" />
-                <StyledTitle>{ title }</StyledTitle>
-            </StyledHeader>
-            <StyledDesc>{ desc }</StyledDesc>
+            <Header>
+                <Image src={img} alt="desc one logo" />
+                <Title>{ title }</Title>
+            </Header>
+            <Desc>{ desc }</Desc>
         </div>
      );
 }
---
+++ src/components/FeatureCard/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+    display: flex;
+    /* justify-content: space-between; */
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+export const Image = styled.img`
+    margin-right: 20px;
+`;
+
+export const Title = styled.h3`
+    margin-top: 11px;
+`;
+
+export const Desc = styled.p`
+    color: #616161;
+    font-size: 14px;
+    font-weight: 300;
+    line-height: 18.9px;
+`;
