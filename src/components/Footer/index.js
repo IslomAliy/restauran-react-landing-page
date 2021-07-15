b1a89f69295610b872e14b64d1b4a911038dabf0
islomaliy <kislombek71@gmail.com>
2021-07-15 19:36:55 +0500

    footer minimized

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,11 @@
-import styled from "styled-components";
-
-const StyledFooterWrapper = styled.div `
-    max-width: 1100px;
-    display: flex;
-    justify-content: space-between;
-    margin: 0 auto;
-    padding: 40px 37px;
-`;
+import {FooterWrapper} from "./style.js";
 
 const Footer = () => {
     return ( 
-        <StyledFooterWrapper>
+        <FooterWrapper>
          <p>Copyright (с) 2019 Ресторан “Летний дворец”</p>
          <p>Facebook — 	Vkontakte — 	Instagram</p>
-        </StyledFooterWrapper>
+        </FooterWrapper>
      );
 }
  
---
+++ src/components/Footer/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.div `
+    max-width: 1100px;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 auto;
+    padding: 40px 37px;
+`;
